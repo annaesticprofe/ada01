fc12638d170b71c33c41caa1ca797355dd2d9fda
annaestic <annaestic@gmail.com>
2022-11-24 11:06:18 +0100

    inici repositori

---
+++ Pildores/javascript/programa.js
@@ -0,0 +1,12 @@
+/* Aquest script, executa un Javascript molt bàsic
+Creadora: Anna Marcilla
+Data: 08/11/2022
+ */
+function myFunction() {
+    var str = "l'anna"; 
+    str = "l'anna\n es professora"; 
+    console.log("1 " + str);
+
+    str = "l'anna es la professora \"de programació\""; 
+    console.log("2 " + str)
+}
